161be8c4a3798cd0aac6ff4f6ebdc4737e51b229
Yuvan Kumar E <109977101+yuvankumar002@users.noreply.github.com>
2022-09-27 21:44:44 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,4 +1,4 @@
-provider "aws"{
+provider "AWS"{
    region  = "ap-south-1" 
    
 }
@@ -8,7 +8,7 @@ resource "aws_instance" "AWSEC2Instance"{
      ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-17"]
-     key_name        = "eyuvankumar"
+     key_name        = "eyuvankumar.pem"
      tags = {
         Name = "tomcatservers"
      }
